1e1088556a7633630cd1f3d70b0c1559e0a355a5
unknown <Dustin.Guillen14@yahoo.com>
2020-05-14 20:06:24 -0600

    test

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const server = express();
 
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+  res.send(`<h2>Let's write some middleware!!!</h2>`);
 });
 
 //custom middleware
